bf7aa28eefcec8d33febbf3a81e6588d4411b9a3
neobombing <60089584+neobombing@users.noreply.github.com>
2023-12-08 08:26:13 +0900

    Update run.sh

--- wallpad/run.sh
+++ wallpad/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 CONFIG_PATH=/data/options.json
-SHARE_DIR=/share
+SHARE_DIR=/homeassistant/share
 
 CUSTOM_FILE=$(jq --raw-output ".customfile" $CONFIG_PATH)
 MODEL=$(jq --raw-output ".model" $CONFIG_PATH)
@@ -24,7 +24,7 @@ fi
 # start server
 echo "[Info] Wallpad Controller stand by... : "$JS_FILE
 
-JS_FILE=/homeassistant/$SHARE_DIR/$JS_FILE
+JS_FILE=/$SHARE_DIR/$JS_FILE
 node $JS_FILE
 
 #while true; do echo "still live"; sleep 1800; done
